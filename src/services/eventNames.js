0c6f2541e9468566d78bfe6aa5ab41d222d769b9
James Wu <therealjameswu@gmail.com>
2019-02-10 17:00:47 -0500

    Add event name enum

---
+++ src/services/eventNames.js
@@ -0,0 +1,12 @@
+const Events = {
+  ERROR: 'ERROR',
+  SKIP: 'SKIP',
+  COMPLETION: 'COMPLETION',
+  BOARDUPDATE: 'BOARDUPDATE',
+  NEXTTURN: 'NEXTTURN',
+  REVEAL: 'REVEAL',
+  STATECHANGE: 'STATECHANGE',
+  END: 'END',
+};
+
+export default Events;
